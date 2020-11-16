2a4728fe54e1da8dd26c6d6bc9b88843eb64e477
Sean Jermey <seanjermey87@gmail.com>
2020-11-16 11:22:50 +0000

    scope package css

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 export default () => {
   return (
-    <div>
-      <StylesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
+    <StylesProvider
+      generateClassName={createGenerateClassName({
+        productionPrefix: "mk",
+      })}
+    >
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
